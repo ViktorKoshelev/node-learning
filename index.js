3e14fee5c8024471d34b3b64be64506b013c6092
ViktorKoshelev <kosha-lev97@yandex.ru>
2021-10-24 22:28:16 +0300

    random number flow

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
-// run `node index.js` in the terminal
+const { pipeline, Readable, Transform, Writable } = require('stream');
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+class RandomNumber extends Readable {
+  count = 0;
+  _read() {
+    this.count++;
+    this.push(this.count == 10 ? null : Math.random().toString());
+  }
+}
+
+class RandomAdder extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Math.random() + Number(chunk.toString());
+    callback(null, transformed.toString());
+  }
+}
+
+class ConsoleLogger extends Writable {
+  _writev(chunks, callback) {
+    console.log(...(chunks.map(({ chunk }) => chunk.toString())));
+    callback();
+  }
+}
+
+pipeline(
+  new RandomNumber(),
+  new RandomAdder(),
+  new ConsoleLogger(),
+  (err) => {
+    if (err) {
+      console.error('Pipeline failed', err);
+    } else {
+      console.log('Pipeline succeded');
+    }
+  }
+)
+
+
+process.on('uncaughtException', (err) => {
+  console.log(err.stack);
+})
+
+class MyError extends Error {
+
+}
+
+throw new MyError('error')
